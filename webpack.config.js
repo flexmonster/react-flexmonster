19d312bc9a1e994c922bc5cf632f8d90dd286b76
Flexmonster Pivot Table <github@flexmonster.com>
2018-11-14 19:11:34 +0200

    react module update

--- webpack.config.js
+++ webpack.config.js
@@ -39,5 +39,6 @@ module.exports = {
   },
   externals: {
     'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+    'react-dom': 'commonjs react-dom'
   }
 };
