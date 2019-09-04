83a10b44c5b3c13f45ee0fbb872c4484fe1fd63f
dima_z <dimazvazhii@gmail.com>
2019-09-04 13:51:08 +0300

    hooks version update

--- src/index.js
+++ src/index.js
@@ -1,68 +1,7 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Flexmonster from "flexmonster";
-
-function PivotHooks(props) {
-	const flexmonsterRef = useRef()
-
-	useEffect(() => {
-		const flexmonster = new Flexmonster({
-			...props,
-			container: flexmonsterRef.current
-		})
-
-		return () => flexmonster.dispose()
-	}, [])
-
-	return <div ref={flexmonsterRef}>Pivot</div>
-}
-
-PivotHooks.propTypes = {
-	afterchartdraw: PropTypes.func,
-	aftergriddraw: PropTypes.func,
-	beforegriddraw: PropTypes.func,
-	beforetoolbarcreated: PropTypes.func,
-	cellclick: PropTypes.func,
-	celldoubleclick: PropTypes.func,
-	componentFolder: PropTypes.string,
-	customizeCell: PropTypes.func,
-	customizeContextMenu: PropTypes.func,
-	datachanged: PropTypes.func,
-	dataerror: PropTypes.func,
-	datafilecancelled: PropTypes.func,
-	dataloaded: PropTypes.func,
-	fieldslistclose: PropTypes.func,
-	fieldslistopen: PropTypes.func,
-	filterclose: PropTypes.func,
-	filteropen: PropTypes.func,
-	fullscreen: PropTypes.func,
-	global: PropTypes.object,
-	height: [PropTypes.string, PropTypes.number],
-	licenseKey: PropTypes.string,
-	loadingdata: PropTypes.func,
-	loadinglocalization: PropTypes.func,
-	loadingolapstructure: PropTypes.func,
-	loadingreportfile: PropTypes.func,
-	localizationerror: PropTypes.func,
-	localizationloaded: PropTypes.func,
-	olapstructureerror: PropTypes.func,
-	olapstructureloaded: PropTypes.func,
-	openingreportfile: PropTypes.func,
-	querycomplete: PropTypes.func,
-	queryerror: PropTypes.func,
-	ready: PropTypes.func,
-	report: [PropTypes.string, PropTypes.object],
-	reportchange: PropTypes.func,
-	reportcomplete: PropTypes.func,
-	reportfilecancelled: PropTypes.func,
-	reportfileerror: PropTypes.func,
-	reportfileloaded: PropTypes.func,
-	runningquery: PropTypes.func,
-	toolbar: PropTypes.bool,
-	update: PropTypes.func,
-	width: [PropTypes.string, PropTypes.number],
-}
 
 export class Pivot extends React.Component {
 	
@@ -106,7 +45,7 @@ Pivot.propTypes = {
 	filteropen: PropTypes.func,
 	fullscreen: PropTypes.func,
 	global: PropTypes.object,
-	height: [PropTypes.string, PropTypes.number],
+	height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 	licenseKey: PropTypes.string,
 	loadingdata: PropTypes.func,
 	loadinglocalization: PropTypes.func,
@@ -120,7 +59,7 @@ Pivot.propTypes = {
 	querycomplete: PropTypes.func,
 	queryerror: PropTypes.func,
 	ready: PropTypes.func,
-	report: [PropTypes.string, PropTypes.object],
+	report: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 	reportchange: PropTypes.func,
 	reportcomplete: PropTypes.func,
 	reportfilecancelled: PropTypes.func,
@@ -129,7 +68,7 @@ Pivot.propTypes = {
 	runningquery: PropTypes.func,
 	toolbar: PropTypes.bool,
 	update: PropTypes.func,
-	width: [PropTypes.string, PropTypes.number],
+	width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 }
 
 export default Pivot;
